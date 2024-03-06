7f865eace3374bbd164601ba45b6a300a32ea3eb
YummyOreo <bobgim20@gmail.com>
2024-03-05 18:00:53 -0600

    feat: move to it's own file

--- update-action/index.js
+++ update-action/index.js
@@ -4,10 +4,10 @@ var fs = require('fs');
 let files = ["../index.html", "../action.html"]
 
 // the code for the featured action
-let replaceContent = ` Help amplify new Time For $5 ads and remind Danaher to put #PeopleOverProfits, by sharing <a href="https://drive.google.com/drive/folders/1-3rQZiWv7d6d217P-pmsPf1RoTpZQL2G" target="_blank">photos of the ads</a> and <a href="https://www.youtube.com/watch?v=v19WQ9MzANM" target="_blank">John's video</a>. <a href="https://docs.google.com/document/d/e/2PACX-1vTh05tEoyCsh8oEWScWM9n41B1pcUeVNGFcUDqdlcILHvVUB2_LsK4voMTdKPdEQwmjMHRBaoalG259/pub" target="_blank">View social media templates</a>.`;
+let replaceContent = fs.readFileSync('./featured-action.html', 'utf8').trim();
 
 for (i in files) {
     let file = files[i]
     let content = fs.readFileSync(file, { encoding: 'utf8' })
-    fs.writeFile(file, content.replaceAll(/<strong>FEATURED ACTION:<\/strong>.*$/gm, "<strong>FEATURED ACTION:</strong>" + replaceContent), () => { })
+    fs.writeFile(file, content.replaceAll(/<strong>FEATURED ACTION:<\/strong>.*$/gm, "<strong>FEATURED ACTION:</strong> " + replaceContent), () => { })
 }
