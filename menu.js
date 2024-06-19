822c801687ae5ea29b4bd57b9cee0eb50d632a90
YummyOreo <bobgim20@gmail.com>
2024-06-19 11:13:10 -0500

    refactor: rename the toggle-buttonto hamburger button to be more descriptive

--- menu.js
+++ menu.js
@@ -1,5 +1,5 @@
 //mobile menu style switcher
-let toggleButton = document.getElementById("toggle-button");
+let toggleButton = document.getElementById("hamburger-button");
 toggleButton.addEventListener("click", (e) => {
 	toggleButton.classList.toggle('active');
 	document.querySelector(".site-menu ul").classList.toggle("active")
