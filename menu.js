12f7f97c35d788471c4c30c0b66c14650ba80a60
YummyOreo <bobgim20@gmail.com>
2025-04-02 16:44:26 -0500

    feat: move some stuff around some more

--- menu.js
+++ menu.js
@@ -1,7 +1,22 @@
 //mobile menu style switcher
-let toggleButton = document.getElementById("hamburger-button");
-toggleButton.addEventListener("click", (e) => {
-	toggleButton.classList.toggle('active');
-	document.querySelector(".site-menu ul").classList.toggle("active")
-	e.preventDefault()
-})
+
+
+if (window.screen.width <= 992) {
+	let toggleButton = document.getElementById("hamburger-button");
+	toggleButton.addEventListener("click", (e) => {
+		toggleButton.classList.toggle('active');
+		document.querySelector(".site-menu ul").classList.toggle("active")
+		e.preventDefault()
+	})
+
+	let dropdownButtons = document.querySelectorAll(".dropdown button");
+	for (let i = 0; i < dropdownButtons.length; i++) {
+		let dropdownButton = dropdownButtons[i];
+		dropdownButton.addEventListener("click", (e) => {
+			dropdownButton.parentNode.querySelector(".dropdown-content").classList.toggle("active");
+			e.preventDefault();
+		})
+	}
+} else {
+
+}
