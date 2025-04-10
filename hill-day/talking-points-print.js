f6fedaf0acea5efd71c3d6868eaaea8973bfae0b
YummyOreo <bobgim20@gmail.com>
2025-04-10 16:57:12 -0500

    feat: add talking points

---
+++ hill-day/talking-points-print.js
@@ -0,0 +1,27 @@
+function printPage() {
+    if (['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(navigator.platform)
+        || (navigator.userAgent.includes("Mac") && "ontouchend" in document)) {
+
+
+        let win = window.open("./talking-points.html", '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
+
+        printWindow.addEventListener('load', function() {
+            printWindow.print();
+            printWindow.close();
+            win.addEventListener("afterprint", (_) => {
+                win.close()
+            }, { once: true });
+        }, true);
+    } else {
+        var iframe = document.createElement('iframe');
+        iframe.style.display = "none";
+        iframe.src = "./talking-points.html";
+        document.body.appendChild(iframe);
+        iframe.contentWindow.onload = () => {
+            iframe.contentWindow.print()
+            iframe.contentWindow.addEventListener("afterprint", (event) => {
+                iframe.remove();
+            })
+        };
+    }
+}
