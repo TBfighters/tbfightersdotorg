e68dab2614417dba59f6c7e4c56ca1456d44b450
Quinn <theyumoreos@gmail.com>
2025-11-08 18:05:59 -0600

    feat: hopeful fixes

--- update-action/index.js
+++ update-action/index.js
@@ -41,6 +41,7 @@ function replaceContent(files, leadingPath, prefixLength) {
         }
 
         if (file.name.endsWith(".html")) {
+            console.log(leadingPath + file.name)
             let content = fs.readFileSync(leadingPath + file.name, { encoding: 'utf8' });
 
             // header
@@ -100,7 +101,7 @@ function replaceContent(files, leadingPath, prefixLength) {
                 }
 
             }
-            fs.writeFile(leadingPath + file.name, content, () => { })
+            // fs.writeFile(leadingPath + file.name, content, () => { })
         }
 
     })
